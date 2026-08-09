4b3f8f642697221b0e8a860512344b1daa9fd4c1
AbdalrahmanEsmat <abdalrahmanesmat22@gmail.com>
2026-08-09 18:41:37 +0300

    creating the rolel management page, adding role management path at app, changing the icon library and adding sidebar-item componets at the global.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Loader from './components/Loader';
 import AdminLayout from './layouts/AdminLayout/AdminLayout';
+import RoleManagement from './pages/RoleManagement';
 
 const JobManagement = lazy(() => import('./pages/JobManagment'));
 
@@ -13,6 +14,7 @@ function App() {
           <Route element={<AdminLayout />}>
             <Route index element={<Navigate to="job-management" replace />} />
             <Route path="job-management" element={<JobManagement />} />
+            <Route path="role-management" element={<RoleManagement />} />
           </Route>
         </Routes>
       </Suspense>
--- src/layouts/AdminLayout/AdminSideBar.tsx
+++ src/layouts/AdminLayout/AdminSideBar.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
-import { PanelLeft } from 'lucide-react';
 import buy2Logo1 from '../../assets/images/buy2logo-1.png';
 import buy2Logo2 from '../../assets/images/buy2logo-2.png';
-import { NavLink } from 'react-router-dom';
+
+import { BriefcaseIcon } from '@heroicons/react/24/solid';
+import { UserGroupIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+
+import { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function AdminSideBar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const { pathname } = useLocation();
 
   return (
     <aside
@@ -29,17 +34,48 @@ export default function AdminSideBar() {
           className={`cursor-pointer ${isCollapsed ? 'border-y border-gray-200 py-2' : ''}`}
           onClick={() => setIsCollapsed((prev) => !prev)}
         >
-          <PanelLeft size={24} className="text-primary-800" />
+          <Bars3Icon className="h-12 w-12 text-primary-800" />
         </button>
       </div>
 
       {/* body  */}
-
       <ul>
-        <li>
-          <NavLink to="job-management">Job Managment</NavLink>
+        <li
+          className={`sidebar-item ${pathname === '/job-management' ? 'sidebar-item-active' : ''}`}
+        >
+          <NavLink to="job-management" className="Link">
+            <BriefcaseIcon
+              className={`h-12 w-12 text-gray-400 ${pathname === '/job-management' ? 'fill-primary-900 ' : ''}`}
+            />
+            {!isCollapsed && (
+              <p
+                className={`text-gray-400 ${pathname === '/job-management' ? 'text-primary-900 font-semibold ' : ''}`}
+              >
+                Job Managment
+              </p>
+            )}
+          </NavLink>
+        </li>
+
+        <li
+          className={`sidebar-item ${pathname === '/role-management' ? 'sidebar-item-active' : ''}`}
+        >
+          <NavLink to="role-management" className="Link">
+            <UserGroupIcon
+              className={`h-12 w-12 text-gray-400 ${pathname === '/role-management' ? 'fill-primary-900 text-primary-900' : ''}`}
+            />
+            {!isCollapsed && (
+              <p
+                className={`text-gray-400 ${pathname === '/role-management' ? 'text-primary-900 font-semibold ' : ''}`}
+              >
+                Role Managment
+              </p>
+            )}
+          </NavLink>
         </li>
       </ul>
+
+      {/* logout button  */}
     </aside>
   );
 }
