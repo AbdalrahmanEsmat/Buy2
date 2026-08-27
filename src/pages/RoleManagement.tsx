04cffe1edcdf1bd49677bcd5da056f6e839f4a0f
EmadRefaat <emad.refaat57@gmail.com>
2026-08-27 13:12:50 +0300

    make navigation to createNewRole page

--- src/pages/RoleManagement.tsx
+++ src/pages/RoleManagement.tsx
@@ -1,12 +1,17 @@
 import { roles } from "@/mocks/db1";
+import { useNavigate } from "react-router-dom";
 
 export default function RoleManagement() {
+  const navigate = useNavigate();
   return (
     <div className="space-y-8">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl font-semibold text-gray-800">Role Based</h2>
 
-        <button className="rounded-lg bg-blue-700 px-5 py-2 text-sm font-medium text-white transition hover:bg-blue-800">
+        <button
+          className="rounded-lg bg-blue-700 px-5 py-2 text-sm font-medium text-white transition hover:bg-blue-800"
+          onClick={() => navigate("/role-management/create-new-role")}
+        >
           Create New Role
         </button>
       </div>
